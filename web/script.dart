3c484bac2c0ea01fe23eb73bb74f62c819ade88a
sbeh <github.com@sbeh.de>
2013-01-25 15:40:19 +0100

    Persist posted messages across page reloads

--- web/script.dart
+++ web/script.dart
@@ -1,30 +1,42 @@
 import 'dart:html';
 import 'dart:async';
 
+import 'lib/local_storage_list.dart';
+
 void main() {
   ButtonElement localSave = query('#localSave');
   ButtonElement localClear = query('#localClear');
   DivElement localMessages = query('#localMessages');
   TextAreaElement message = query('#message');
 
+  var localList = new LocalStorageList('local');
+
+  localList.asList.forEach((m) => addMessageToDOM(localMessages, m));
+  localClear.disabled = localList.asList.isEmpty;
+
   localSave.on.click.add((e) {
-    if(!message.value.trim().isEmpty) {
-      var m = message.value.trim();
-      m = m.replaceAll(new RegExp('\\r?\\n'), '<br>');
-
-      var child = new DivElement();
-      child.classes.add('message');
-      child.innerHtml = m;
-
-      localMessages.children.add(child);
-      localClear.disabled = false;
+    if(!message.value.trim().isEmpty)
+      localList.add(message.value.trim());
+    else {
+      message.value = '';
+      localSave.disabled = true;
     }
+  });
+
+  localList.on('add', (m) {
+    addMessageToDOM(localMessages, m);
 
     message.value = '';
     localSave.disabled = true;
+
+    localClear.disabled = false;
   });
 
   localClear.on.click.add((e) {
+    localList.clear();
+  });
+
+  localList.on('clear', () {
     localMessages.children.clear();
     localClear.disabled = true;
   });
@@ -32,4 +44,14 @@ void main() {
   new Timer.repeating(500, (t) {
     localSave.disabled = message.value.trim().isEmpty;
   });
+}
+
+addMessageToDOM(DivElement container, String message) {
+  message = message.replaceAll(new RegExp('\\r?\\n'), '<br>');
+
+  var child = new DivElement();
+  child.classes.add('message');
+  child.innerHtml = message;
+
+  container.children.add(child);
 }
